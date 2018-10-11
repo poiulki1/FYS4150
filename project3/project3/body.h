016caa74efde107b4c0779d45d2feaca5c9982df
Adam Nieweglowski <6flint@gmail.com>
2018-10-11 13:42:48 +0200

    implemented classes vec3 and body

--- project3/project3/body.h
+++ project3/project3/body.h
@@ -1,15 +1,31 @@
 #ifndef BODY_H
 #define BODY_H
 
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <vector>
+using std::vector;
 
 class body
 {
 public:
-    body(arma::vec pos, arma::vec vel, double mass);
-    double b_mass;
-    arma::vec b_position;
-    arma::vec b_velocity;
-    arma::vec acceleration = arma::vec<arma::zeros>(2);
+    //Properties
+    double mass;
+    double position[3];
+    double velocity[3];
+    double potential_energy;
+    double kinetic_energy;
+
+    //Initializers
+    body();
+    body(double M, double x, double y, double z, double vx, double vy, double vz);
+
+    //Functions
+    double distance(body other_body);
+    double grav_force(body other_body, double g_constant);
+    double acceleration(body other_body, double g_constant);
+    double kinEnergy();
+    double potEnergy(body &other_body, double g_constant, double eps);
 };
 
 #endif // BODY_H
